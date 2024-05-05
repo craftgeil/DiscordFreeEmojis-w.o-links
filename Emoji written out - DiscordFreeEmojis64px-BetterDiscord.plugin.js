66ff66d967c5a7df63f38c5ab299213dddebdbfa
craftgeil <80261988+craftgeil@users.noreply.github.com>
2024-05-05 18:48:59 +0200

    Update Emoji written out - DiscordFreeEmojis64px-BetterDiscord.plugin.js
    fix

--- Emoji written out - DiscordFreeEmojis64px-BetterDiscord.plugin.js
+++ Emoji written out - DiscordFreeEmojis64px-BetterDiscord.plugin.js
@@ -142,8 +142,8 @@ function Start() {
         return result;
     }
 
-    function replaceEmoji(parseResult, emoji) {
-        parseResult.content = parseResult.content.replace(`<${emoji.animated ? "a" : ""}:${emoji.originalName || emoji.name}:${emoji.id}>`, "[" + `${emoji.animated ? "" : ""}${emoji.originalName || emoji.name}` + "](" + emoji.url.split("?")[0] + "?size=64)");
+     function replaceEmoji(parseResult, emoji) {
+        parseResult.content = parseResult.content.replace(`<${emoji.animated ? "a" : ""}:${emoji.originalName || emoji.name}:${emoji.id}>`,"[" + emoji.name + "](" + `https://cdn.discordapp.com/emojis/${emoji.id}.${emoji.animated ? 'gif' : 'webp'}?size=64` + ")") ;
     }
 
     parseHook = function() {
